48b0db4f66eb81cec645bfac23dd5cb1ff3272d1
migiright <migigit@yahoo.co.jp>
2017-08-01 21:48:35 +0900

    モールス信号に変換する関数追加

--- morse.c
+++ morse.c
@@ -1,5 +1,6 @@
 #include "morse.h"
 #include "button.h"
+#include <ctype.h>
 
 #define DAH_TIME 200
 
@@ -47,3 +48,65 @@ void morse_update() {
 char morse_signal(){
 	return signal;
 }
+
+const char * const MorseAlphabets[] = {
+	"･-", "-･･･", "-･-･", "-･･", "･", "･･-･", "--･", //ABCDEFG
+	"････", "･･", "･---", "-･-", "･-･･", "--", "-･", //HIJKLMN
+	"---", "･--･", "--･-", "･-･", "･･･", "-", "･･-", //OPQRSTU
+	"･･･-", "･--", "-･･-", "-･--", "--･･", //VWXYZ
+};
+
+const char * const MorseNumerals[] = {
+	"-----", "･----", "･･---", "･･･--", "････-", //01234
+	"･････", "-････", "--･･･", "---･･", "----･", //56789
+};
+
+const char * const MorsePeriod = "･-･-･-";
+const char * const MorseComma = "--･･--";
+const char * const MorseQuestion = "･･--･･";
+const char * const MorseColon = "---･･･";
+const char * const MorseSemicolon = "-･-･-･";
+const char * const MorseHyphen = "-･･･-";
+const char * const MorseNone = 0;
+
+const char *morseFromChar(char c){
+	if(isalpha(c)){
+		return MorseAlphabets[toupper(c) - 'A'];
+	} else if(isdigit(c)){
+		return MorseNumerals[c - '0'];
+	} else {
+		switch(c){
+		case '.': return MorsePeriod;
+		case ',': return MorseComma;
+		case '?': return MorseQuestion;
+		case ':': return MorseColon;
+		case ';': return MorseSemicolon;
+		case '-': return MorseHyphen;
+		default: return MorseNone;
+		}
+	}
+}
+
+void convertStrToMorse(const char *str, char *dst, int len) {
+	const char * const end = dst + len - 1; //'\0'を書き込む場所
+	for(; *str != '\0'; ++str){
+		if(*str == ' '){ //スペースはスペース1個あける
+			if(dst == end) goto full;
+			*dst++ = ' ';
+		} else if(morseFromChar(*str) == 0){ //対応していない文字は飛ばす
+			continue;
+		} else {
+			//*strの文字に対応するモールス信号を書き込む
+			for(const char *pChar = morseFromChar(*str); *pChar != '\0'; ++pChar){
+				if(dst == end) goto full;
+				*dst++ = *pChar;
+			}
+			//スペース一個開ける
+			if(dst == end) goto full;
+			*dst++ = ' ';
+		}
+	}
+full:
+	*dst = '\0';
+}
+
--- morse.h
+++ morse.h
@@ -9,3 +9,10 @@ void morse_update(); //Systick1回ごとに呼ぶ
 //トンかツーが確定したときのみDITかDAHが返ってくる
 //それ以外はSPACEが返ってくる
 char morse_signal();
+
+//文字に対応するモールス信号を返す
+const char *morseFromChar(char c);
+
+//モールス信号に変換する
+//len: dstに何文字書き込むか('\0'含む)
+void convertStrToMorse(const char *str, char *dst, int len);
