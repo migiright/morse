cab3cdedf6affd7bfbadcf49f639dc7c7738d969
migiright <migigit@yahoo.co.jp>
2017-08-01 19:51:40 +0900

    トンかツーか判定して表示する

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 //#include "stm32f30x.h"
 //#include "stm32f30x_rcc.h"
 #include "lcd.h"
+#include "button.h"
+#include "morse.h"
+
+void main_systickHandler()
+{
+	morse_update();
+	char buf[] = " ";
+	buf[0] = morse_signal();
+	if(buf[0] == DAH || buf[0] == DIT){
+		lcd_str(buf);
+	}
+}
 
 int main(void)
 {
@@ -8,6 +20,9 @@ int main(void)
 	SysTick_Config(SystemCoreClock/1000);
 
 	lcd_init();
+	button_init();
+
+	lcd_position(0, 0);
 
 	while(1){
 	}
